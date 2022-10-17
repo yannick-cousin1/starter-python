84b6738d1c0bc3ca877c38d78aa67f63e3b2e020
yannick-cousin1 <yannick.cousin@laplateforme.io>
2022-10-17 17:24:16 +0200

    add job12

---
+++ job12.py
@@ -0,0 +1,22 @@
+#regex mod
+import re
+
+#function to return length of a list
+def length(list):
+	c = 0
+	for j in list:
+		c += 1
+	return c
+
+#open file and read
+with open("data.txt") as file:
+	content = file.read()
+#Note sure if it's considered cheating :
+#	number = content.split()
+#	print ("Nombre de mots = ", length(number))
+# else :
+
+#using regex to isolate all words in a list
+	words = re.findall(r"[\w-]+", content)
+	print ("Nombre de mots = ", length(words))
+	
