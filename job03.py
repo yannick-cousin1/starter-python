0cbbe8e2b5ab422ac3ea397ebb33b139aec0d9ba
yannick-cousin1 <yannick.cousin@laplateforme.io>
2022-10-17 09:29:46 +0200

    add job03

---
+++ job03.py
@@ -0,0 +1,9 @@
+
+age = int(input("Bonjour, quel âge as tu ? "))
+
+if age >= 18:
+	print ("Tu es majeur")
+elif age < 18:
+	print ("Tu es mineur")
+else:
+	print("erreur, il faut rentrer un nombre")
