a1550d41397a6aa1e1fa6e4c7fd655ede581012c
yannick-cousin1 <yannick.cousin@laplateforme.io>
2022-10-18 16:07:02 +0200

    add comments to job23

--- job23.py
+++ job23.py
@@ -1,4 +1,4 @@
-
+#function to return length of a string/list
 def length(string):
 	c = 0
 	for i in string:
@@ -6,15 +6,18 @@ def length(string):
 	return c
 
 word = input("Entrez un mot : ")
-
+#create a list containing the string
 LIST = []
 tmp = ''
 for i in word:
 	LIST.append(i)
 
+#if last char is equal to the char right before it, do nothing and go to the next step of the loop
+#at the next step, we compare char -2 and -3. We'll do that till they are different ( to swap them )
 for j in range(1,length(LIST)):
 	if LIST[-j] == LIST[-1-j]:
 		print ('', end = '')
+#if it's not, then swap them and break
 	else:
 		tmp = LIST[-1-j]
 		LIST[-1-j] = LIST[-j]
