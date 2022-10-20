97a6b074fbb7b769e2b0ef5844bf2c94d44b682c
yannick-cousin1 <yannick.cousin@laplateforme.io>
2022-10-20 07:42:31 +0200

    correct minor problem in job11bis

--- job11bis.py
+++ job11bis.py
@@ -5,11 +5,12 @@ LIST = []
 #open the file domains.xml
 with open("domains.xml") as file:
 	x = file.readlines()
-	   						# x is a list. Each value is a line of the file
-	for i in x:				# For each line
-		if "domain" in i:	# If the line contain the string "domain"
-			LIST.append(i)	# Add it to a new list
-			c += 1			# count it
+	   											# x is a list. Each value is a line of the file
+	for i in x:									# For each line
+		if "domain" in i and "column" in i:	  	# If the line contains the string "domain" and "column" 
+												# ( One line contains "domain" but there is no domain in it)
+			LIST.append(i)						# Add it to a new list
+			c += 1								# count it
 
 	for j in range(len(LIST)):				#Just another loop to compare every domains
 		for k in range(j+1,len(LIST)):		#in order to decrease counter if a domain is in the list twice
